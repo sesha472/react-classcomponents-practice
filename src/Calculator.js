76ebed83c83d1237c4cd22e04f4719ce3cf74882
sesha <sesha472@gmail.com>
2023-11-18 23:10:35 +0530

    cal file code added

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 
-const Calculator = () => {
 
+const Calculator = () => {
+  const [data, setdata] = useState("")
+  const [result, setresult] = useState("")
+  const handler=(e)=>{
+    
+    setdata(e.target.value);
+  }
     
   return (
     <div >
         <h1>Calculator</h1>
+
+        <input type='text' onChange={handler} /><br/>
+        <button onClick={()=>setresult(eval(data))}>result</button><br/>
+        <button onClick={()=>{ setresult("")}}>reset</button><br/>
+        {result}
         
         
         
