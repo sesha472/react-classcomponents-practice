4e73975985057c2f712e39a7c096eb50fe9a1429
sesha <sesha472@gmail.com>
2023-11-20 20:41:53 +0530

    getting data from fake json using axios library

--- src/asserts/Getdatafromurl.js
+++ src/asserts/Getdatafromurl.js
@@ -1,13 +1,19 @@
-import React, { useEffect ,useState} from 'react'
+import React, { useEffect ,useState} from 'react';
+import axios from 'axios';
 
 const Getdatafromurl = () => {
     const [data, setdata] = useState([]);
     useEffect(()=>{
-        fetch("https://jsonplaceholder.typicode.com/users").then
-        ( response=>response.json()).then
-            (result=>setdata(result))
 
-    },[data])
+        // fetch("https://jsonplaceholder.typicode.com/users").then
+        // ( response=>response.json()).then
+        //     (result=>setdata(result))
+        // we can do same thing using axios by installing the libabry 
+         axios.get("https://jsonplaceholder.typicode.com/users").then(
+            response=>setdata(response.data) 
+         )
+
+    },[])
     
   return (
     <div>
